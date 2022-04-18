d5e8c4d00e60e1b342e770df3d39b9b2ca99a626
joaozz21 <joao.aof14@gmail.com>
2022-04-18 20:18:53 -0300

    api

--- Benfazer -Fragmets/app/src/main/java/com/generation/benfazerproject/util/Constants.kt
+++ Benfazer -Fragmets/app/src/main/java/com/generation/benfazerproject/util/Constants.kt
@@ -1,5 +1,5 @@
 package com.generation.benfazerproject.util
 
 object Constants {
-    const val BASE_URL = "https://mobileecoceum.herokuapp.com/"
+    const val BASE_URL = "https://mobileecoum.herokuapp.com/"
 }
